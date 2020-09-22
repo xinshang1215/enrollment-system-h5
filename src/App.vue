399eb59bd84eb23c0de47f14b4ee6f636a6c9993
xinshang1215 <94335897@qq.com>
2020-09-22 16:19:36 +0800

    添加文件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 
-
-/*
- * @Author: M.M
- * @Date:   2020-08-31 14:27:24
- * @Last Modified by:   M.M
- * @Last Modified time: 2020-08-31 16:06:44
- */
-
-
 <template>
   <div id="app">
-  <h1>fjdafkljdkl</h1>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    
-    </div>
     <router-view/>
+    <Bar></Bar>
   </div>
 </template>
+<script>
+import Bar from "@/components/Bar";
 
+export default {
+  name:"app",
+  comments:[
+    Bar
+  ],
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
 <style lang="scss">
 @import "styles/base.scss";
 </style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+name: "bar.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
